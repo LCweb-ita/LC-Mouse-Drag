dc6c3aac1402c959baa833d42f577864f04684ce
Luca <l.montanari@lcweb.it>
2025-12-16 15:32:35 +0100

    v1.1.0
    - new: better events pattern tracked to avoid troubles with unreliabble classic mouse ones

--- lc-mouse-drag.js
+++ lc-mouse-drag.js
@@ -1,37 +1,36 @@
 /**
  * lc-mouseDrag.js - Vanilla javascript (ES6) function enabling drag scrolling on desktop
- * Version: v1.0.2
+ * Version: v1.1.0
  * Author: Luca Montanari (LCweb)
  * Website: https://lcweb.it
  * Licensed under the MIT license
  */
 
-(function() { 
-   "use strict";
+(function() {
+    "use strict";
 
-    /* prevent multiple script inits */ 
-    if(typeof(window.lc_mouseDrag) == 'function') {
+    /* prevent multiple script inits */
+    if (typeof window.lc_mouseDrag === 'function') {
         return true;
     }
 
-
+    
     /* 
      * Public class initializing the plugin for each targeted element 
      * suggested ratio = 0.3
      */
     window.lc_mouseDrag = function(attachTo, ratio = 0.3, ignoreX = false, ignoreY = false) {
-        if(!attachTo) {
+        if (!attachTo) {
             return console.error('You must provide a valid selector or DOM object as first argument');
         }
 
         
         /* get elements to attach event to */
         const get_elems = function(selector) {
-            if(typeof(selector) != 'string') {
-                return (selector instanceof Element) ? [selector] : Object.values(selector);   
+            if(typeof selector !== 'string') {
+                return (selector instanceof Element) ? [selector] : Object.values(selector);
             }
 
-            // clean problematic selectors
             (selector.match(/(#[0-9][^\s:,]*)/g) || []).forEach(function(n) {
                 selector = selector.replace(n, '[id="' + n.replace("#", "") + '"]');
             });
@@ -41,44 +40,88 @@
 
         
         /* perform */
-        const mouseDrag = function($elem) { 
-            let trackX = (!ignoreX) ? true : false,
-                trackY = (!ignoreY) ? true : false,
+        const mouseDrag = function($elem) {
+            let trackX = !ignoreX,
+                trackY = !ignoreY,
 
-                curDown = false,
-                curYPos = 0,
-                curXPos = 0,
+                isDragging = false,
+                pointerId = null,
 
-                startScrollY 	= 0,
-                startScrollX 	= 0,
-                scrollDif   	= 0,
-                animation       = null;    
+                startYPos = 0,
+                startXPos = 0,
 
+                startScrollY = 0,
+                startScrollX = 0,
 
-            $elem.addEventListener('mousedown', (e) => {
-                e.preventDefault();
-                curDown = true;
+                animation = null;
 
-                startScrollY = parseInt($elem.scrollTop, 10);
-                startScrollX = parseInt($elem.scrollLeft, 10);
-                curYPos = e.clientY;
-                curXPos = e.clientX;
-            }); 
-
-
-            $elem.addEventListener('mouseup', (e) => {
-                if(!ratio) {
-                    return true;    
+            
+            /* pointer down */
+            $elem.addEventListener('pointerdown', (e) => {
+                if(e.pointerType !== 'mouse') {
+                    return;
                 }
-                
-                // smooth scroll
+                e.preventDefault();
+
+                isDragging = true;
+                pointerId = e.pointerId;
+
+                startScrollY = $elem.scrollTop;
+                startScrollX = $elem.scrollLeft;
+
+                startYPos = e.clientY;
+                startXPos = e.clientX;
+
+                $elem.setPointerCapture(pointerId);
+            });
+
+            
+            /* pointer move */
+            $elem.addEventListener('pointermove', (e) => {
+                if(!isDragging || e.pointerId !== pointerId) {
+                    return;
+                }
+
+                if(animation) {
+                    animation.pause();
+                }
+
+                let scroll_obj = {
+                    behavior: 'auto'
+                };
+
+                if(trackY) {
+                    scroll_obj.top = startScrollY + (startYPos - e.clientY);
+                }
+                if(trackX) {
+                    scroll_obj.left = startScrollX + (startXPos - e.clientX);
+                }
+
+                $elem.scroll(scroll_obj);
+            });
+
+            
+            /* pointer up / cancel */
+            const endDrag = (e) => {
+                if(!isDragging || e.pointerId !== pointerId) {
+                    return;
+                }
+
+                isDragging = false;
+                $elem.releasePointerCapture(pointerId);
+                pointerId = null;
+
+                if(!ratio) {
+                    return;
+                }
+
                 let currScrollY = $elem.scrollTop,
                     scrollDiffY = (startScrollY - currScrollY) * -1,
-                    newScrollY = currScrollY + ( scrollDiffY * ratio),
+                    newScrollY = currScrollY + (scrollDiffY * ratio),
 
                     currScrollX = $elem.scrollLeft,
                     scrollDiffX = (startScrollX - currScrollX) * -1,
-                    newScrollX = currScrollX + ( scrollDiffX * ratio);
+                    newScrollX = currScrollX + (scrollDiffX * ratio);
 
                 let scroll_obj = {
                     behavior: 'smooth'
@@ -91,43 +134,18 @@
                 }
 
                 animation = $elem.scroll(scroll_obj);
-            });
+            };
 
-            
-            
-            document.body.addEventListener('mouseup', (e) => {
-                curDown = false;
-            });
-            
-            
-
-            $elem.addEventListener('mousemove', (e) => {
-                if(curDown === true) {
-                    if(animation) {
-                        animation.pause();    
-                    }
-
-                    let scroll_obj = {
-                        behavior: 'auto'
-                    };
-                    if(trackY) {
-                        scroll_obj.top = startScrollY + (curYPos - e.clientY);
-                    }
-                    if(trackX) {
-                        scroll_obj.left = startScrollX + (curXPos - e.clientX);
-                    }
-                    
-                    $elem.scroll(scroll_obj);
-                }
-            });
+            $elem.addEventListener('pointerup', endDrag);
+            $elem.addEventListener('pointercancel', endDrag);
         };
-        
 
+        
         // init
         get_elems(attachTo).forEach(($el) => {
 
-            // only for devices not having a mouse
-            if(window.matchMedia('(pointer: fine)').matches == false) {
+            // run only on devices with a real mouse
+            if(window.matchMedia('(pointer: fine)').matches === false) {
                 document.body.classList.add('lc_mousedrag_is_touch');
                 return true;
             }
@@ -135,4 +153,4 @@
             mouseDrag($el);
         });
     };
-})();    
+})();
